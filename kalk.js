3fed5b00200781ce785639d2dcca7c5722f81dcd
ZStachura <zmstachura@gmail.com>
2021-12-08 20:40:00 +0100

    2.1 -> New options
    -> Added Help Pop-out
    ->Added Multiply function

--- kalk.js
+++ kalk.js
@@ -42,13 +42,62 @@ function getInput(storageName = 'inputtstorage') {
 	  //Utworzenie obieralnej listy
 	  for(var i=g;i<store.length;i++){
 	  var opt=document.createElement('option');
-	  opt.value=opt.text=store[i];
-	  document.getElementById("history").appendChild(opt);
-	  console.log("Input value:",document.getElementById("input").value)	  
+	  opt.value=store[i];
+	  opt.index=i;
+	  document.getElementById("history").appendChild(opt); 
 	}
 	g=i;
-
   }
+
+  function Replace()
+  {
+	const element = document.getElementById("history");
+	document.getElementById("B(").disabled=true
+	document.getElementById("R").disabled=false
+	document.getElementById("P").disabled=false
+	document.getElementById("M").disabled=false
+	//const checkValue = element.options[selectedIndex];
+	firstper=[]
+	secondper=[]
+	//inputper=checkValue;
+	console.log(document.getElementById("p_input")) 	 
+  }
+
+
+  function onTwoPermutations(inputp)
+  {
+	  var second=false
+	  for(var i=1;i<inputp.length;i+=2)
+	  {
+		 if(second!=true)
+		 {
+			firstper.push(inputp[i])
+		 }
+		 else
+		 {
+			 secondper.push(inputp[i])
+		 }
+		 if(inputp[i+1]==')')
+		 {
+			 second=true
+			 i+=1
+		 }
+	  }
+	  console.log("Input: ",inputp)
+	  console.log("First perm: ",firstper)
+	  console.log("Second perm: ",secondper)
+  }
+
+function Help(){
+	var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }   
+}
+
 //BV->Button Value, BId -> Button Id
 function IN(BV, BId){
 
@@ -109,7 +158,7 @@ function IN(BV, BId){
 		//Zapisanie danych do Local Storage
 		saveInput(inputper)
 	}
-	document.getElementById("input").value = inputper;
+	document.getElementById("p_input").value = inputper;
 };
 
 function Reverse()
@@ -147,57 +196,162 @@ function Multiply()
 	var SecondDown=Array.from(secondper);
 	SecondDown.push(SecondDown[0]);
 	SecondDown.shift()
-	console.log("Firstper:",FirstUp)
-	console.log(FirstDown)
-	
+	multiplyperm="("
+	var result=[]
+	i=0
+	while(i!=secondper.length){
+		var final=[]
+		final.push(SecondUp[i])
+		if(i==SecondUp.length-1)
+		{
+			if(FirstUp.includes(SecondUp[0]))
+			{
+				j=0;
+				while(j!=FirstUp.length)
+				{
+					if(j==FirstUp.length-1)
+					{
+						if(FirstUp[j]==SecondUp[0])
+						{
+							final.push(FirstUp[0])
+							break
+						}
+					}
+					else
+					{
+						if(FirstUp[j] == SecondUp[0])
+						{
+							final.push(FirstUp[j+1])
+							break;
+						}
+					}
+					j+=1;
+				}				
+			}
+			else
+			{
+				final.push(SecondUp[0])
+			}
+		}
+		else
+		{
+			if(FirstUp.includes(SecondUp[i+1]))
+			{
+				j=0;
+				while(j!=FirstUp.length)
+				{
+					if(j==FirstUp.length-1)
+					{
+						if(FirstUp[j]==SecondUp[i+1])
+						{
+							final.push(FirstUp[0])
+							break
+						}
+					}
+					else
+					{
+						if(FirstUp[j] == SecondUp[i+1])
+						{
+							final.push(FirstUp[j+1])
+							break;
+						}
+					}
+					j+=1;
+				}				
+			}
+			else
+			{
+				final.push(SecondUp[i+1])
+			}
+		}
+		if(!if_same(final,result))
+		{
+			result.push(JSON.parse(JSON.stringify(final)))
+			if(multiplyperm[multiplyperm.length-3]==final[0])
+			{
+				multiplyperm=multiplyperm.substring(0,multiplyperm.length-2);
+				multiplyperm+=","
+				multiplyperm+=multiplyperm[multiplyperm.length-1]
+			}
+			else
+			{
+				multiplyperm+=final
+			}
+			multiplyperm+=")("
+		}
+		i+=1;
+	}
+	if(multiplyperm[multiplyperm.length-1]=="(")
+	{
+		multiplyperm=multiplyperm.substring(0,multiplyperm.length-1)
+	}
+	else
+	{
+		multiplyperm+=")"
+	}
+	document.getElementById("m_output").value=multiplyperm;
 
+}
+
+function if_same(final,permutation)
+{
+	var same=false
+	for(var i=0;i<final.length;i++)
+	{
+		for(var j=0; j<permutation.length;j++)
+		{
+			for(var k=0;k<final[i].length;k++)
+			{
+				if(final[i][k]==permutation[j])
+				{
+					same=true
+					break
+				}
+				else
+				{
+					same=false
+				}
+			}
+		}
+		if(same)
+		{
+			break
+		}
+	}
+	return same
 }
 
 function Power()
 {
 	var powerperm=" ";
-	if(multiplyperm!='')
+	powerperm+="(";
+	for(let i=0;i<firstper.length;i+=2)
 	{
-		powerperm+="(";
-		for(let i=0;i<multiplyperm.length-1;i++)
-		{
-			powerperm+=multiplyperm[i];
-			powerperm+=","
-		}
-		powerperm+=multiplyperm[multiplyperm.length-1];
-		powerperm+=")";
+		powerperm+=firstper[i];
+		powerperm+=","
 	}
-	else
+	for(let k=1;k<firstper.length;k+=2)
+	{
+		powerperm+=firstper[k];
+		if(k+2<firstper.length){
+		powerperm+=","}
+	}
+	powerperm+=")";
+	if(secondper!=' ')
 	{
 		powerperm+="(";
-		for(let i=0;i<firstper.length;i+=2)
-		{
-			powerperm+=firstper[i];
-			powerperm+=","
-		}
-		for(let k=1;k<firstper.length;k+=2)
-		{
-			powerperm+=firstper[k];
-			if(k+2<firstper.length){
-			powerperm+=","}
-		}
-		powerperm+=")";
-		if(secondper!=' ')
-		{
-			powerperm+="(";
-		for(let i=0;i<secondper.length;i+=2)
-		{
-			powerperm+=secondper[i];
-			powerperm+=","
-		}
-		for(let k=1;k<secondper.length;k+=2)
-		{
-			powerperm+=secondper[k];
-			if(k+2<secondper.length){
-			powerperm+=","}
-		}
-		powerperm+=")";
-		}
+	for(let i=0;i<secondper.length;i+=2)
+	{
+		powerperm+=secondper[i];
+		powerperm+=","
+	}
+	for(let k=1;k<secondper.length;k+=2)
+	{
+		powerperm+=secondper[k];
+		if(k+2<secondper.length){
+		powerperm+=","}
+	}
+	powerperm+=")";
 	}
 	document.getElementById("p_output").value=powerperm;
 }
@@ -213,7 +367,7 @@ function clean()
     document.getElementById("R").disabled=false
 	document.getElementById("P").disabled=false
 	document.getElementById("M").disabled=true
-    document.getElementById("input").value = "";
+    document.getElementById("p_input").value = "";
     document.getElementById("m_output").value = "";
     document.getElementById("r_output").value = "";
     document.getElementById("p_output").value = "";
